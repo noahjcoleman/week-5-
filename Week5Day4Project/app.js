1c181a370c5e4330a7e1b48b367f2d574a62ef98
noah coleman <noahjcoleman@gmail.com>
2020-11-19 18:28:00 -0700

    This is my jQuery project ready for submission

---
+++ Week5Day4Project/app.js
@@ -0,0 +1,14 @@
+let navbar = $(".navItem");
+$(".navItem").css('color', 'blue');
+
+let nav = $('#nav');
+$("#nav").css('background-color', '#145');
+
+let even = $('li').even();
+$(even).css('background-color', '#ccc');
+
+let odd = $('li').odd();
+$(odd).css('background-color', '#aaa');
+
+let inputs = $(':input');
+alert(inputs.val("Noah"));
